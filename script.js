2c63caec5e4224cef79e3e703c6892433391c7e4
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-22 15:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,20 +31,29 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
+function rot13(str) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-  for (let i = 0; i < encodedStr.length; i++) {
-    const char = encodedStr[i];
-    const decodedChar = lookup[char] || char; // Lookup decoded character or use the original character if not found
-    decodedArr.push(decodedChar);
-  }
+	for(let i=0;i<str.length;i++){
+		if(lookup[str[i]]===undefined){
+			decodedArr.push(str[i]);
+		}
+		else{
+			decodedArr.push(lookup[str[i]]);
+		}
+		
+		
+	}
 
-  return decodedArr.join(""); // Join the decoded characters into a string
+  return decodedArr; //return decodedArr
 }
 
-// Testing the function with the given input
-console.log(rot13("SERR YBIR? NPPVBWBO")); // Output: "FREE LOVE? ACCCCCCOB"
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// Exporting the function for testing purposes
-module.exports = rot13;
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+// Do not change this line
+window.rot13 = rot13;
+
+
+
