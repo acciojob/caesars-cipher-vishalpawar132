a18b01601eb92314715edc971a43d39904fe69cc
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-24 12:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function rot13(str) {
 		
 	}
 
-  return decodedArr.join().replaceAll(",", ""); //return decodedArr
+  return decodedArr.join(""); //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
